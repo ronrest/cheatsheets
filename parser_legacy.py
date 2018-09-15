199c6b7e90c97cd1c92565b9dd81bab12f9bdd26
ronrest <ronny.coding@gmail.com>
2018-09-15 22:27:27 +1000

    FEAT: (ACTUALLY) move legacy parser to parser_legacy.py

---
+++ parser_legacy.py
@@ -0,0 +1,25 @@
+import re
+
+file = "test"
+with open(file) as fileObj:
+    text = fileObj.read()
+
+
+SECTION_PATTERN = "\n#{10,}\n\s*(.*)\n#{10,}\n"
+SECTION_REPLACEMENT = "\n<h2>{0}</h2>\n"
+
+
+count = 0
+match = re.search(SECTION_PATTERN, text)
+if match:
+    while match:
+        count += 1
+        print count
+        substitution = SECTION_REPLACEMENT.format(*match.groups())
+        text_before = text[:match.start()]
+        text_after = text[match.end():]
+        text = text_before + substitution + text_after
+        match = re.search(SECTION_PATTERN, text)
+
+
+print text
